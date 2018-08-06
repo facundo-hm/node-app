dc8f2527ba6a24a4dcb3ae5efa5e384afa56cd95
Facundo HM <flacundo@gmail.com>
2018-08-06 19:23:16 -0300

    Change User schema to class

--- models/User.js
+++ models/User.js
@@ -4,48 +4,62 @@ const mongoose = require('mongoose'),
       jwt = require('jsonwebtoken'),
       secret = require('../config').secret;
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    lowercase: true,
-    unique: true,
-    required: [true, 'Should be defined'],
-    match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
-    index: true
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    unique: true,
-    required: [true, 'Should be defined'],
-    match: [/\S+@\S+\.\S+/, 'is invalid'],
-    index: true
-  },
-  type: {
-    type: String,
-    lowercase: true,
-    required: [true, 'Should be defined'],
-    index: true
-  },
-  hash: String,
-  salt: String
-}, { timestamps: true });
+class UserSchema extends mongoose.Schema {
+  constructor(schema, options) {
+    const userSchema = Object.assign({
+      username: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, 'Should be defined'],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        index: true
+      },
+      email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, 'Should be defined'],
+        match: [/\S+@\S+\.\S+/, 'is invalid'],
+        index: true
+      },
+      type: {
+        type: String,
+        lowercase: true,
+        required: [true, 'Should be defined'],
+        index: true
+      },
+      hash: String,
+      salt: String
+    }, schema);
+  
+    const userOptions = Object.assign({ timestamps: true }, options);
 
-UserSchema.plugin(uniqueValidator, { message: 'already exists.' });
+    super(userSchema, userOptions);
 
-const methods = {
-  validPassword: function (password) {
+    this.plugin(uniqueValidator, { message: 'already exists.' });
+
+    this.method({
+      validPassword: this.validPassword,
+      setPassword: this.setPassword,
+      generateJWT: this.generateJWT,
+      toAuthJSON: this.toAuthJSON,
+      toProfileJSONFor: this.toProfileJSONFor
+    });
+  }
+
+  validPassword (password) {
     const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 
     return this.hash === hash;
-  },
+  }
 
-  setPassword: function (password) {
+  setPassword (password) {
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
-  },
+  }
 
-  generateJWT: function () {
+  generateJWT () {
     const today = new Date();
     const exp = new Date(today);
   
@@ -57,23 +71,23 @@ const methods = {
       type: this.type,
       exp: parseInt(exp.getTime() / 1000),
     }, secret);
-  },
+  }
 
-  toAuthJSON: function () {
+  toAuthJSON () {
     return {
       username: this.username,
       email: this.email,
       token: this.generateJWT()
     };
-  },
+  }
 
-  toProfileJSONFor: function (user) {
+  toProfileJSONFor (user) {
     return {
       username: this.username
     };
   }
-};
+}
 
-UserSchema.method(methods);
+const User = new UserSchema();
 
-mongoose.model('User', UserSchema);
+mongoose.model('User', User);
